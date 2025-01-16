e0b3faa60ebc1bb808a429fbeb88ce2d7734fc47
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2025-01-16 11:04:09 -0500

    Cambios

--- Ejemplos/src/ejemplos/Ejemplo03.java
+++ Ejemplos/src/ejemplos/Ejemplo03.java
@@ -12,27 +12,34 @@ import java.security.SecureRandom;
  *
  * @author reroes
  */
-public class Ejemplo01 {
+public class Ejemplo03 {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        int valorA = obtenerNumero(); // 3
-        int valorB = obtenerNumero(); // 2
-        int suma = obtenerSuma(valorA, valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n", 
-                valorA,
-                valorB,
-                suma);
+        
+        int numeroElementos = 20;
+        int limite = 100;
+        int [] valores = obtenerValores(numeroElementos, limite);
+
+        
+        for (int i = 0; i < valores.length; i++) {
+            System.out.printf("%d ", valores[i]);
+        }
+        System.out.println("\n");
     }
     
-    public static int obtenerNumero() {
+    public static int[] obtenerValores(int a, int b) {
         // objeto generador de números aleatorios
         SecureRandom numerosAleatorios = new SecureRandom();
         
         // Returns a pseudorandom, uniformly distributed int value 
         // between 0 (inclusive) and the specified value (exclusive)
-        int valorAleatorio = numerosAleatorios.nextInt(5);
-        return valorAleatorio;
+        int [] valores = new int [a];
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = numerosAleatorios.nextInt(b);
+        }
+        
+        return valores;
     }
     
     public static int obtenerSuma(int a, int b){
